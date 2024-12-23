6e4667ff32d4b5ea1be6050c896fd8e3de2828d7
john1340a <jean-kelly.seguin@outlook.fr>
2024-12-23 22:32:08 +0100

    Remove admin routes for user management

--- back/routes/web.php
+++ back/routes/web.php
@@ -7,10 +7,4 @@ Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
 
-Route::middleware(['auth', 'admin'])->prefix('admin')->name('admin.')->group(function () {
-    Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
-    Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
-    Route::post('/users/{user}/maps', [UserController::class, 'addMap'])->name('users.maps.store');
-});
-
 require __DIR__.'/auth.php';
